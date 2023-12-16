3e47e7e2b874da2a4544208501c0f75f91d718a3
Leah DeYoung <deyounlm@gmail.com>
2023-12-15 21:25:01 -0500

    updates

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import folium
 import geopy
 from geopy.distance import great_circle
-import deap
 import random
-#import mlrose
 import numpy as np
 import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 import json
+import plotly.graph_objects as go
+from dash.dependencies import Output
 
 df_final_cities = pd.read_csv('/https://raw.githubusercontent.com/LDeYoung17/jingle-jam-2023/ds-work-branch/notebooks/final_cities.csv', encoding = "iso-8859-1")
 
@@ -17,8 +16,6 @@ def calculate_distance(location1, location2):
 
 number_list = [0,1,2,3,4,5,6,7,8]
 
-random.shuffle(number_list)
-print(number_list)
 df_final_cities['number'] = number_list
 
 def fitness(route):
@@ -35,6 +32,10 @@ def fitness(route):
 
         total_distance += calculate_distance(lat_long_1, lat_long_2)
     return total_distance
+
+baseline_route = [0,1,2,3,4,5,6,7,8]
+
+baseline_model = fitness(baseline_route)
 
 def create_population():
     location_numbers = list(range(9))
@@ -81,7 +82,6 @@ population_size = 10
 mutation_rate = 0.1  
 best_route_per_generation = []
 
-# Initialize population with random routes
 selection_population = create_population()
 
 for generation in range(num_generations):
@@ -103,39 +103,125 @@ for generation in range(num_generations):
     best_route_per_generation.append((best_route, best_route_fitness))
 
 best_overall_route = min(best_route_per_generation, key=lambda x: x[1])
-print(best_overall_route)
+
+best_of_routes = best_overall_route[0]
+optimized_route_data = []
+
+for k in best_of_routes:
+    city_row = df_final_cities[df_final_cities['number'] == k]
+    city_data = {
+        'city': city_row['City'].iloc[0],
+        'latitude': city_row['Latitude'].iloc[0],
+        'longitude': city_row['Longitude'].iloc[0]
+    }
+    optimized_route_data.append(city_data)
+
+df_optimized_route = pd.DataFrame(optimized_route_data)
+
+mapbox_access_token = open(".mapbox_token").read()
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H4('Polotical candidate voting pool analysis'),
-    html.P("Select a candidate:"),
+    html.H4('Mapping Deliveries Around the World'),
     dcc.RadioItems(
-        id='candidate', 
-        options=["Joly", "Coderre", "Bergeron"],
-        value="Coderre",
+        id='map', 
+        options=["Scatter", "Route Map"],
+        value="Route Map",
         inline=True
     ),
     dcc.Graph(id="graph"),
+
 ])
 
 
 @app.callback(
-    Output("graph", "figure"), 
-    Input("candidate", "value"))
-def display_choropleth(candidate):
-    df = px.data.election() # replace with your own data source
-    geojson = px.data.election_geojson()
-    fig = px.choropleth(
-        df, geojson=geojson, color=candidate,
-        locations="district", featureidkey="properties.district",
-        projection="mercator", range_color=[0, 6500])
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    Output("graph", "figure"),
+    Input("map", "value")) 
+
+
+def display_trace_scattermapbox(selected_map):
+    if selected_map == 'Route Map':
+        fig = go.Figure(go.Scattermapbox(
+        ))
+
+        fig.add_trace(go.Scattermapbox(
+            mode = "markers+lines",
+            lon = df_final_cities['Longitude'],
+            lat = df_final_cities['Latitude'],
+            text = df_final_cities['City'],
+            name='Baseline Route',
+            marker = {'size': 10})),
+
+        fig.add_trace(go.Scattermapbox(
+            mode = "markers+lines",
+            lon = df_optimized_route['longitude'],
+            lat = df_optimized_route['latitude'],
+            text = df_optimized_route['city'],
+            name = 'Optimized Route',
+            marker = {'size': 10}))
+
+        fig.update_layout(
+            margin ={'l':0,'t':0,'b':0,'r':0},
+            mapbox = {
+                'center': {'lon': 10, 'lat': 10},
+                'style': "open-street-map",
+                'center': {'lon': -20, 'lat': -20},
+                'zoom': 1})
+    elif selected_map == 'Scatter':
+        fig = go.Figure(go.Scattermapbox(
+            lat=df_final_cities['Latitude'],
+            lon=df_final_cities['Longitude'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=9
+            ),
+            text=df_final_cities['City'],
+        ))
+
+        fig.update_layout(
+            autosize=True,
+            hovermode='closest',
+            mapbox=dict(
+                accesstoken=mapbox_access_token,
+                bearing=0,
+                center=dict(
+                    lat=38.92,
+                    lon=-77.07, 
+            ),
+            pitch=0,
+            zoom=1
+            ),
+        )
     return fig
 
 
 app.run_server(debug=True)
 
 
+data_to_convert = {
+    "data_config": {
+        "csv_file_path": "/Users/admin/Desktop/GitHub/new_repos/jingle-jam-2023/notebooks/final_cities.csv",
+        "number_list": number_list,
+        "population_size": 10,
+        "num_generations": 250,
+        "mutation_rate": 0.1
+    },
+    "best_route": {
+        "route": best_overall_route[0],
+        "fitness": best_overall_route[1],
+        "baseline_model": baseline_model,
+        "baseline_route": baseline_route
+    },
+    "optimized_route_data": optimized_route_data,
+    "dash_app_layout": {
+        "title": "Mapping Deliveries Around the World",
+        "radio_items_options": ["Route Map", "Scatter"],
+        "default_value": "Route Map"
+    }
+}
 
+json_data = json.dumps(data_to_convert, indent=4)
+
+with open('output.json', 'w') as json_file:
+    json.dump(data_to_convert, json_file, indent=4)
